8380762fb6a4e03b1574159d615a807ae9284b97
Алексеев Алексей <Alale20002504@gmail.com>
2021-03-18 07:19:43 +0500

    Added update in background

--- MobileApps/MobileApps/ViewModels/ReportsViewModel.cs
+++ MobileApps/MobileApps/ViewModels/ReportsViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Windows.Input;
 using MobileApps.Interfaces;
 using MobileApps.Models;
@@ -10,27 +11,69 @@ namespace MobileApps.ViewModels
     public class ReportsViewModel : BaseViewModel
     {
         private Page _ownPage;
-        public ObservableCollection<IReport> Reports { get; }
+        public ObservableCollection<IReport> Reports => App.CurrentUser.Reports;
+        public IReport SelectedReport { get; set; }
+
+        private BackgroundWorker _bwUpdater;
 
         public ReportsViewModel(Page page)
         {
             _ownPage = page;
-            Reports ??= new ObservableCollection<IReport>();
-            Reports.Add(new Report(new Car("A123BC", "186", "RUS"), new ObservableCollection<string>()));
-            Reports.Add(new Report(new Car("A123BC", "186", "RUS"), new ObservableCollection<string>()));
-            Reports.Add(new Report(new Car("A123BC", "186", "RUS"), new ObservableCollection<string>()));
-            Reports.Add(new Report(new Car("A345BC", "86", "RUS"), new ObservableCollection<string>()));
-            Reports.Add(new Report(new Car("A345BC", "86", "RUS"), new ObservableCollection<string>()));
+            _bwUpdater = new BackgroundWorker
+            {
+                WorkerReportsProgress = true,
+            };
+            _bwUpdater.DoWork += BwUpdaterOnDoWork;
+            _bwUpdater.ProgressChanged += BwUpdaterOnProgressChanged;
+            _bwUpdater.RunWorkerCompleted += BwUpdaterOnRunWorkerCompleted;
+            IsBusy = true;
+            _bwUpdater.RunWorkerAsync();
         }
 
-        public ICommand MoreInfoReportCommand => new Command<IReport>(x =>
+        private void BwUpdaterOnRunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            IsBusy = false;
+            OnPropertyChanged(nameof(Reports));
+
+        }
+
+        private void BwUpdaterOnProgressChanged(object sender, ProgressChangedEventArgs e)
         {
             
-        }, x => true);
+        }
+
+        private void BwUpdaterOnDoWork(object sender, DoWorkEventArgs e)
+        {
+            App.CurrentUser.UpdateReports("http://188.225.83.42:7000");
+        }
+
+        private bool _isBusy;
+
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set
+            {
+                _isBusy = value;
+                OnPropertyChanged(nameof(IsBusy));
+            }
+        }
+        public ICommand RefreshInfoCommand => new Command(() =>
+        {
+            //App.CurrentUser.UpdateReports("http://188.225.83.42:7000");
+            _bwUpdater.RunWorkerAsync();
+        });
+
+        public ICommand MoreInfoReportCommand => new Command(() =>
+        {
+            _ownPage.Navigation.PushModalAsync(new DetailReportInfoPage(SelectedReport));
+            SelectedReport = null;
+        });
 
         public ICommand OpenNewReportPageCommand => new Command(() =>
         {
             _ownPage.Navigation.PushModalAsync(new NewReportPage());
         });
+
     }
 }
