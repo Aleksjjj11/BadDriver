c0b59b3aa8fd262ecc9310575dd3160770fe0302
Алексеев Алексей <Alale20002504@gmail.com>
2021-03-18 07:14:15 +0500

    Added user to test

--- MobileApps/MobileApps/App.xaml.cs
+++ MobileApps/MobileApps/App.xaml.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using MobileApps.Interfaces;
+using MobileApps.Models;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -6,6 +12,7 @@ namespace MobileApps
 {
     public partial class App : Application
     {
+        public static IUser CurrentUser;
         public App()
         {
             InitializeComponent();
@@ -15,14 +22,40 @@ namespace MobileApps
 
         protected override void OnStart()
         {
+            if (Preferences.ContainsKey("username") && Preferences.ContainsKey("password"))
+            {
+                if (Connectivity.NetworkAccess == NetworkAccess.Internet)
+                {
+                    CurrentUser = new User
+                    {
+                        Username = Preferences.Get("username", "nick"),
+                        Password = Preferences.Get("password", "27062000")
+                    };
+                    CurrentUser.Update("http://188.225.83.42:7000");
+                    CurrentUser.UpdateReports("http://188.225.83.42:7000");
+                }
+            }
+            else
+            {
+                CurrentUser = new User
+                {
+                    Username = "nick",
+                    Password = "27062000",
+                };
+                CurrentUser.Update("http://188.225.83.42:7000");
+                CurrentUser.UpdateReports("http://188.225.83.42:7000");
+            }
         }
 
         protected override void OnSleep()
         {
+            Preferences.Set("username", CurrentUser.Username);
+            Preferences.Set("password", CurrentUser.Password);
         }
 
         protected override void OnResume()
         {
+            
         }
     }
 }
